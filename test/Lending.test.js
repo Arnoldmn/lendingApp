8c9a5c3da141df35b0125522f1f25546ecbfdfbe
arnoldmn <arnoldnzibuk@gmail.com>
2024-02-07 07:42:17 +0300

    Should allow borrow test

---
+++ test/Lending.test.js
@@ -0,0 +1,29 @@
+const MicroLending = artifacts.require("MicroLending");
+
+contract('Micro lending', (accounts) => {
+    let microLeningInstance;
+
+    const owner = accounts[0];
+    const borrower = accounts[1];
+
+    beforeEach(async () => {
+        microLeningInstance = await MicroLending.new({ from: owner });
+
+    });
+
+    it("Should allow users to request a loan", async () => {
+        const amount = Web3.utils.toWei("1", "ether");
+        const interestRate = 5;
+        const daysToRepay = 7;
+
+        await microLeningInstance.requestLoan(amount, interestRate, daysToRepay, { from: borrower });
+
+        const loan = await microLendingInstance.loans(i);
+
+        assert.equal(loan.borrower, borrower);
+        assert.equal(loan.amount.toString(), amount);
+        assert.equal(loan.interestRate.toString(), interestRate());
+        assert.equal(loan.dueDate > 0, true);
+        assert.equal(loan.reapaid, false);
+    })
+})
