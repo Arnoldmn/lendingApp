8117c8ea63c873abc3248604a112c794aed75d5a
arnoldmn <arnoldnzibuk@gmail.com>
2024-02-07 07:58:06 +0300

    Should repayloan test

--- test/Lending.test.js
+++ test/Lending.test.js
@@ -26,4 +26,24 @@ contract('Micro lending', (accounts) => {
         assert.equal(loan.dueDate > 0, true);
         assert.equal(loan.reapaid, false);
     })
+
+    it("Should allow owner to repay a loan", async () => {
+        const amount = Web3.utils.toWei("1", "ether");
+        const interestRate = 5;
+        const daysToRepay = 7;
+
+
+        await microLeningInstance.requestLoan(amount, interestRate, daysToRepay, {from: borrower});
+
+        const initialBalance = await Web3.eth.getBalance(owner);
+        const loan = await microLendingInstance.loans(i);
+
+        await microLeningInstance.repaidLoan(i, { value: loan.amount * (100 + loan.interestRate)/ 100 });
+
+        const finalBalance = await Web3.eth.getBalance(owner);
+        const repaidLoan = await microLendingInstance.loans(i);
+
+        assert.equal(repaidLoan.repaid, true);
+        assert.equal(finalBalance > initialBalance, true);
+    })
 })
